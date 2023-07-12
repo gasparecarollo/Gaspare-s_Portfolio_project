33399f0b3a5d93b230a68348ac25eedf01361de4
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-07-12 18:14:12 -0400

    added a video src in pokeCtr.html

--- index.js
+++ index.js
@@ -10,16 +10,37 @@ select_teamname.eventListener("change"), (event) => {
     else { document.getElementById("bgi").style.backgroundImage = "url('Assets/selectateam.jpeg')"; }
 }
 
-fetch("https://pokeapi.co/api/v2/pokemon/")
+const options = {
+    method: 'Get',
+    headers: {
+        'X-RapidAPI-Key': '740ed91875msh031c2a94c5bac10p1ea857jsnc595262bf611',
+        'X-RapidAPI-Host': 'pokemon-go1.p.rapidapi.com'
+    }
+
+};
+
+fetch("https://pokemon-go1.p.rapidapi.com/type_effectiveness.json")
     .then(response => response.json())
     .then((JSONresponse) => {
         console.log(JSONresponse)
         const fetchedPokemon = JSONresponse
+
+        console.log(fetchedPokemon)
     })
     .catch((err) => {
         console.log(err)
     })
 
+
+//Positioning Nav menu li by ID
+
+// const getHome = document.getElementById("home");
+// const getPokeCtr = document.getElementById("pokectr");
+// const getPokeMart = document.getElementById("pokemart");
+// const getPokeLounge = document.getElementById("pokelounge");
+
+// getHome.style.left = "146px";
+// getHome.style.top = "46px";
 
 
 
