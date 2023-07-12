fdca9ef6c89096b9717437d619aa43fae68bca19
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-07-12 12:54:59 -0400

    added form

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const select_teamname = document.querySelector("#teamname");
+select_teamname.eventListener("change"), (event) => {
+
+    teamChosen = document.getElementById("teamname");
+
+    if (teamChosen = none) { document.getElementById("teamname").style.backgroundColor = "red"; }
+    else if (teamChosen === "Instinct") { document.getElementById("teamname").style.backgroundImage = "url('Assets/4024146-middle.png')"; }
+    else if (teamChosen === "Mystic") { document.getElementById("teamname").style.backgroundImage = "url('Assets/4024146-middle.png')"; }
+    else if (teamChosen === "Valor") { document.getElementById("teamname").style.backgroundImage = "url('Assets/4024146-middle.png')"; }
+    else { document.getElementById("bgi").style.backgroundImage = "url('')"; }
+}
+
+
+
+
+
 fetch("https://pokeapi.co/api/v2/pokemon/")
     .then(response => response.json())
     .then((JSONresponse) => {
@@ -7,3 +23,15 @@ fetch("https://pokeapi.co/api/v2/pokemon/")
     .catch((err) => {
         console.log(err)
     })
+
+
+
+
+function displayCard() {
+
+
+
+
+}
+
+
