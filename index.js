cc1e6ca0bbe3df712aa47409b3fae6cc42b09390
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-07-12 22:51:51 -0400

    Added the Fetch for pokemon types and strengths and weaknesses

--- index.js
+++ index.js
@@ -1,35 +1,97 @@
+const test = console.log;
+
+const dontBodyShameMe = document.querySelector("body");
+const mainSection = document.querySelector("main");
+const headingOne = document.querySelector("h1");
+const newForm = document.getElementById("myForm");
+const submit = document.getElementById("submitBtn");
+// const reset = document.getElementById("resetBtn");
+const allFormInputs = document.querySelectorAll("#stockForm input");
+const unorderedList = document.querySelector("ul");
+const getTrainerName = document.getElementById("playername");
+const getLevel = document.getElementById("level");
+const getQR = document.getElementById("imageUrl");
+
+const friendList = document.getElementById("friendList");
+const inventList = document.getElementById("inventListSect");
+
+
+document.getElementById("myForm").addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    fetch("https://pokeapi.co/api/v2/type")
+        .then(response => response.json())
+        .then(data => {
+            const fetchedTypes = data.results;
+
+            fetchedTypes.forEach(type => {
+                fetch(type.url)
+                    .then(response => response.json())
+                    .then(typeData => {
+                        const typeName = typeData.name;
+                        const damage = typeData.damage_relations;
+                        const strengths = damage.double_damage_to.map(item => item.name);
+                        const weaknesses = damage.double_damage_from.map(item => item.name)
+                        const counters = {
+                            strengths, weaknesses
+                        };
+                        console.log(`Counters for ${type.name}`)
+                        console.log(counters)
+
+                        displayCard(typeName, counters);
+                    })
+                    .catch((error) => test(error));
+            });
+        }).catch((error) => test(error));
+});
+
+function displayCard(typeName, counters) {
+    const cardSection = document.createElement("article");
+    cardSection.classList.add("card");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = typeName;
+
+    const strengthsH3 = document.createElement("h3");
+    strengthsH3.textContent = "Strengths";
+
+    const strengthsList = document.createElement("ul");
+    counters.strengths.forEach(strength => {
+        const strengthItem = document.createElement("li");
+        strengthItem.textContent = strength;
+        strengthsList.appendChild(strengthItem);
+    })
+    const weaknessesH3 = document.createElement("h3");
+    weaknessesH3.textContent = "Weaknesses";
+    const weaknessesList = document.createElement("ul");
+    counters.weaknesses.forEach(weakness => {
+        const weaknessItem = document.createElement("li");
+        weaknessItem.textContent = weakness;
+        weaknessesList.appendChild(weaknessItem);
+    });
+
+    cardSection.append(h2, strengthsH3, strengthsList, weaknessesH3, weaknessesList);
+    mainSection.append(cardSection);
+}
+
+
+
+
 const select_teamname = document.querySelector("#teamname");
-select_teamname.eventListener("change"), (event) => {
+select_teamname.addEventListener("change", (event) => {
 
-    teamChosen = document.getElementById("teamname");
+    teamChosen = document.getElementById("teamname").value;
 
-    if (teamChosen = none) { document.getElementById("teamname").style.backgroundColor = "red"; }
+    if (teamChosen === "none") { document.getElementById("teamname").style.backgroundColor = "red"; }
     else if (teamChosen === "Instinct") { document.getElementById("teamname").style.backgroundImage = "url('Assets/4024146-middle.png')"; }
     else if (teamChosen === "Mystic") { document.getElementById("teamname").style.backgroundImage = "url('Assets/4024146-middle.png')"; }
     else if (teamChosen === "Valor") { document.getElementById("teamname").style.backgroundImage = "url('Assets/4024146-middle.png')"; }
     else { document.getElementById("bgi").style.backgroundImage = "url('Assets/selectateam.jpeg')"; }
-}
+});
 
-const options = {
-    method: 'Get',
-    headers: {
-        'X-RapidAPI-Key': '740ed91875msh031c2a94c5bac10p1ea857jsnc595262bf611',
-        'X-RapidAPI-Host': 'pokemon-go1.p.rapidapi.com'
-    }
 
-};
 
-fetch("https://pokemon-go1.p.rapidapi.com/type_effectiveness.json")
-    .then(response => response.json())
-    .then((JSONresponse) => {
-        console.log(JSONresponse)
-        const fetchedPokemon = JSONresponse
-
-        console.log(fetchedPokemon)
-    })
-    .catch((err) => {
-        console.log(err)
-    })
 
 
 //Positioning Nav menu li by ID
@@ -44,11 +106,6 @@ fetch("https://pokemon-go1.p.rapidapi.com/type_effectiveness.json")
 
 
 
-function displayCard() {
 
-
-
-
-}
 
 
